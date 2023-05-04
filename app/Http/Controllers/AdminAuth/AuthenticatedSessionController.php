cc0432ecf38e76db5eb6932f4cfbf221b6a186a0
mahmoud-saber <60474033+mahmoud-saber@users.noreply.github.com>
2023-05-04 14:17:14 +0300

    customizind admin login

--- app/Http/Controllers/AdminAuth/AuthenticatedSessionController.php
+++ app/Http/Controllers/AdminAuth/AuthenticatedSessionController.php
@@ -25,7 +25,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
-        $request->authenticate();
+        $request->authenticate('admin');
 
         $request->session()->regenerate();
 
