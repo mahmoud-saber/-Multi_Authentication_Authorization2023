1cbe7e9b621c72c912164ce083b871150b2bb78e
mahmoud-saber <60474033+mahmoud-saber@users.noreply.github.com>
2023-05-05 14:53:08 +0300

    install spatie and customizing sidebar and model admin

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,17 +2,18 @@
 
 namespace App\Models;
 
-use App\Notifications\AdminPasswordNotification;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
  use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Notifications\AdminPasswordNotification;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admin extends Authenticatable 
+class Admin extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable ,HasRoles;
 
     public function sendPasswordResetNotification($token)
     {
--- routes/web.php
+++ routes/web.php
@@ -28,8 +28,8 @@ require __DIR__ . '/auth.php';
 // ///////////////////////////////////////////
 // back design
 Route::prefix('back')->name('back.')->group(function () {
-    Route::get('/', BackHomeController::class)->middleware('admin')->name('index');
-
+    Route::get('/', BackHomeController::class)->name('index');
+    // middleware('admin')->
 
     require __DIR__ . '/adminauth.php';
 });
@@ -38,13 +38,3 @@ Route::prefix('back')->name('back.')->group(function () {
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
